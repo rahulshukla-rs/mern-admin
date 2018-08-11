08047145ce0ed154faca9ea9ba7ee4819d368534
Rahul Shukla <16shuklarahul@gmail.com>
2018-08-11 22:31:06 +0530

    login with passport & jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
   mongoURI: "mongodb://admin:admin123@ds123929.mlab.com:23929/mern_admin"
+  //mongoURI: "mongodb://localhost:27017/mern_admin",
+  jwtKEY: "mern-admin"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const keys = require("../../config/keys");
 
 // Load user Model
 const User = require("../../models/User");
@@ -20,7 +23,9 @@ router.get("/test", (req, res) =>
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists." });
+      return res
+        .status(400)
+        .json({ status: "0", msg: "Email already exists." });
     } else {
       const newUser = new User({
         name: req.body.name,
@@ -41,5 +46,59 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// @route POST api/users/login
+// @desc Login User / Return JWT Token
+// @access public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find User by Email
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ status: "0", msg: "user not found." });
+    }
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //res.json({ status: "1", msg: "Login Success" });
+        //User Matched
+        const payLoad = {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }; //Create JWT PayLoad
+
+        // Sign Token
+        jwt.sign(payLoad, keys.jwtKEY, { expiresIn: 3600 }, (err, token) => {
+          res.json({
+            status: "1",
+            token: "Bearer " + token
+          });
+        });
+      } else {
+        return res
+          .status(400)
+          .json({ status: "0", msg: "Incorrect Password." });
+      }
+    });
+  });
+});
+
+// @route GET api/users/current
+// @desc Return current User
+// @access private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
